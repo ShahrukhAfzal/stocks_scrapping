08e4d00c3e6a319276b82476b953b7ca0bf6951e
shahrukh <shoaibtayyab121@gmail.com>
2020-12-26 17:38:11 +0530

    Scraping done for BSE and NSE (not saved in db)

--- ws_moneycontrol/ws_moneycontrol/items.py
+++ ws_moneycontrol/ws_moneycontrol/items.py
@@ -8,5 +8,14 @@ import scrapy
 
 class WsMoneycontrolItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
     pass
+
+class StockMarketItem(scrapy.Item):
+    company = scrapy.Field()
+    ltp = scrapy.Field()
+    change_percent = scrapy.Field()
+    volume = scrapy.Field()
+    buy_price = scrapy.Field()
+    sell_price = scrapy.Field()
+    buy_qty = scrapy.Field()
+    sell_qty = scrapy.Field()
---
+++ ws_moneycontrol/ws_moneycontrol/spiders/stockmarket_spider.py
@@ -0,0 +1,63 @@
+import scrapy
+
+class StockMarketSpider(scrapy.Spider):
+    name = 'stocks'
+    start_urls = [
+        'https://www.moneycontrol.com/markets/indian-indices/'
+    ]
+
+    def parse(self, response, **kwargs):
+        return self.parse_pages(response)
+
+    def parse_pages(self, response):
+        #Bombay Stock Exchange
+        bse_all_pages = response.xpath('//*[(@id = "collapse1")]/*[@class="accordion_list"]/li/a/@href').extract()
+        for url in bse_all_pages:
+            try:
+                yield response.follow(url, callback=self.get_all_bse)
+            except:
+                pass
+
+        nse_all_pages = response.xpath('//*[(@id = "collapse2")]/*[@class="accordion_list"]/li/a/@href').extract()
+        for url in nse_all_pages:
+            try:
+                yield response.follow(url, callback=self.get_all_nse)
+            except:
+                pass
+
+
+    def get_all_bse(self, response):
+        table = response.xpath('//*[(@id = "nsebse_1")]//*[@class="responsive"]//tbody//tr')
+        for row in table:
+            data = row.xpath('td//text()').extract()
+            data_dict = {
+                            'company': data[0],
+                            'ltp': data[1],
+                            'change_percent': data[2],
+                            'volume': data[3],
+                            'buy_price': data[4],
+                            'sell_price': data[5],
+                            'buy_qty': data[6],
+                            'sell_qty': data[7],
+                            'market': "Bombay Stock Exchange",
+                        }
+
+            yield data_dict
+
+    def get_all_nse(self, response):
+        table = response.xpath('//*[(@id = "nsebse_1")]//*[@class="responsive"]//tbody//tr')
+        for row in table:
+            data = row.xpath('td//text()').extract()
+            data_dict = {
+                            'company': data[0],
+                            'ltp': data[1],
+                            'change_percent': data[2],
+                            'volume': data[3],
+                            'buy_price': data[4],
+                            'sell_price': data[5],
+                            'buy_qty': data[6],
+                            'sell_qty': data[7],
+                            'market': "National Stock Exchange",
+                        }
+
+            yield data_dict
